98750b0c4b133965632f7b61afd143648daa9fe2
linh96011@st.vimaru.edu.vn <linh96011@st.vimaru.edu.vn>
2025-09-09 08:55:00 +0700

    fix database

--- admin/index.php
+++ admin/index.php
@@ -2,8 +2,5 @@
 <?php include './inc/dashboard.php'; ?>
 <div class="main-content">
     Welcome Admin Panel
-    <?php
-        var_dump($_SESSION);
-    ?>
 </div>
 <?php include './inc/footer.php'; ?>
